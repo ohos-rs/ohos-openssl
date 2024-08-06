52ad3e5c7c2930aa035cf28ee39fba0c772434b1
richerfu <southorange0929@foxmail.com>
2024-08-06 10:25:06 +0800

    feat: add crate

---
+++ crates/build.rs
@@ -0,0 +1,18 @@
+use std::env;
+
+fn main() {
+    let current_dir = env::current_dir().unwrap();
+    let aarch = (&current_dir).join("prelude/arm64-v8a/");
+    let arm = (&current_dir).join("prelude/armeabi-v7a/");
+    let x64 = (&current_dir).join("prelude/x86_64/");
+
+    env::set_var("AARCH64_UNKNOWN_LINUX_OHOS_OPENSSL_DIR", aarch.to_str().unwrap());
+    env::set_var("ARMV7_UNKNOWN_LINUX_OHOS_OPENSSL_DIR", arm.to_str().unwrap());
+    env::set_var("X86_64_UNKNOWN_LINUX_OHOS_OPENSSL_DIR", x64.to_str().unwrap());
+
+    println!("cargo:warning=hello\n\n\n");
+
+    println!("cargo:rustc-env=AARCH64_UNKNOWN_LINUX_OHOS_OPENSSL_DIR={}", aarch.to_str().unwrap());
+    println!("cargo:rustc-env=ARMV7_UNKNOWN_LINUX_OHOS_OPENSSL_DIR={}", arm.to_str().unwrap());
+    println!("cargo:rustc-env=X86_64_UNKNOWN_LINUX_OHOS_OPENSSL_DIR={}", x64.to_str().unwrap());
+}
---
+++ scripts/publish-crate.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -rf "${PWD}/crates/ohos-openssl/prelude"
+
+cp -r "${PWD}/prelude" "${PWD}/crates/ohos-openssl"
+
